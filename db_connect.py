6515a5278223663ae1dcf7ca14b6b028211e75dc
hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2020-09-06 20:18:05 +1200

    add db_connect.py

---
+++ db_connect.py
@@ -0,0 +1,20 @@
+"""
+Author: Justin
+Description: see db_connect.__doc__
+"""
+import sqlite3
+
+
+def db_connect(db_path="main.db"):
+    """Connect to a database, set the isolation level to None, and return the connection object.
+    
+    Keyword arguments:
+    db_path -- the path to the database to connect to (default main.db)
+    """
+    conn = sqlite3.connect(db_path)
+    conn.isolation_level = None
+    return conn
+
+
+if __name__ == "__main__":
+    db_connect()
