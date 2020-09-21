4d92eabcb3a10c086fc996f5cadafc670ececa0f
hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2020-09-22 11:21:53 +1200

    update indenting

--- db_create.py
+++ db_create.py
@@ -6,53 +6,53 @@ db_path = "main.db"
 
 
 def db_create(db_path="main.db"):
-  """Check that the database doesn't exist, create the database, create the
-  tables, finally connect to the database.
+    """Check that the database doesn't exist, create the database, create the \
+    tables, finally connect to the database.
 
-  Use os.path.isfile() on the database path to check if the file exists.
-  Connect to the database. Set the conn.isolation_level to None.
-  If the database doesn't exist create the tables leaderboard and score with
-  the columns username, date; and leaderboard_id, and score.
-  Else connect to the database.
-  """
-  if not isfile(db_path):
-    conn = sqlite3.connect(db_path)
-    conn.isolation_level = None
-    c = conn.cursor()
-    c.execute("""
+    Use os.path.isfile() on the database path to check if the file exists.
+    Connect to the database. Set the conn.isolation_level to None.
+    If the database doesn't exist create the tables leaderboard and score with
+    the columns username, date; and leaderboard_id, and score.
+    Else connect to the database.
+    """
+    if not isfile(db_path):
+        conn = sqlite3.connect(db_path)
+        conn.isolation_level = None
+        c = conn.cursor()
+        c.execute("""
     CREATE TABLE leaderboard(
       username varchar(15) NOT NULL,
       date date NOT NULL
     );
     """)
-    c.execute("""
+        c.execute("""
     CREATE TABLE score(
       leaderboard_id REFERENCES leaderboard(rowid) NOT NULL,
       score tinyint(2) NOT NULL
     )
     """)
-    current_date = date.today()
-    users = [['Billy', current_date], ['Eilish', current_date],
-             ['Joel', current_date]]
-    score = [[5, 7, 15], [15, 15, 14], [2, 7, 13]]
-    for i in users:
-      c.execute(
-          """
+        current_date = date.today()
+        users = [['Billy', current_date], ['Eilish', current_date],
+                 ['Joel', current_date]]
+        score = [[5, 7, 15], [15, 15, 14], [2, 7, 13]]
+        for i in users:
+            c.execute(
+                """
       INSERT into leaderboard (username,date) VALUES (?,?);
       """, [i[0], i[1]])
-      user_id = c.lastrowid
-      for i in range(3):
-        c.execute(
-            """
+            user_id = c.lastrowid
+            for i in range(3):
+                c.execute(
+                    """
         INSERT into score (leaderboard_id,score) VALUES (?,?);
         """, [user_id, score[users.index(i)][i]])
-    return conn, c
-  else:
-    conn = sqlite3.connect(db_path)
-    conn.isolation_level = None
-    c = conn.cursor()
-    return conn, c
+        return conn, c
+    else:
+        conn = sqlite3.connect(db_path)
+        conn.isolation_level = None
+        c = conn.cursor()
+        return conn, c
 
 
 if __name__ == "__main__":
-  db_create(db_path)
+    db_create(db_path)
