450fb23291ed179d7a17270773162e6f1b5d0638
hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2020-10-20 12:49:04 +1300

    add logic

--- quiz_check.py
+++ quiz_check.py
@@ -1,10 +1,18 @@
 """See quiz_check.__doc__."""
 
 
-def quiz_check(location):
-    pass
+def quiz_check(location, score_one, score_two, score_three):
+    """Check if there is an unplayed quiz in the users current room."""
+    if ((location == 1 and score_one is not None)
+            or (location == 2 and score_two is not None)
+            or (location == 3 and score_three is not None)):
+        return True
+    return False
 
 
 if __name__ == "__main__":
-    quiz_check.__doc__(int(input("""
-    Location: """)))
+    print(
+        quiz_check(int(input("""
+    Location: """)), None, int(input("""
+    Score two: """)), int(input("""
+    Score three: """))))
