4caca0aed4eaf25385ec264283de62198d6af314
hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2020-10-27 12:24:59 +1300

    add main logic

--- quiz.py
+++ quiz.py
@@ -1,10 +1,45 @@
 """See quiz.__doc__."""
 
+from random import randint as rand
 
-def quiz(location):
-    pass
+
+def quiz(location, score):
+    """D."""
+    quiz_questions = [[
+        """Write one of:
+    Example
+    None
+    wasd""", """Fill in the *:
+    Boom clap I'm in me mom's *"""
+    ], [], []]
+    quiz_answers = [[["Example", "None", "wasd"], ["car"]], [], []]
+    current_questions = quiz_questions[location - 1]
+    current_answers = quiz_answers[location - 1]
+    score[location - 1] = 0
+    while len(current_questions) > 0:
+        rand_choice = rand(0, len(current_questions) - 1)
+        user_input = input(f"""
+    {current_questions.pop(rand_choice)}
+    : """)
+        answers = current_answers.pop(rand_choice)
+        if user_input in answers:
+            input("""
+    You got it right""")
+            score[location - 1] += 1
+        else:
+            answers_text = ""
+            for i in answers:
+                answers_text += f"    {i},\n"
+            answers_text = "".join(answers_text.rsplit(
+                ",", 1))  # reverse version of string.replace()
+            input(f"""
+    You got it wrong.
+    The answer was one of:
+{answers_text}""")
+    return (score[0], score[1], score[2])
+    text = ''
+    text.replace()
 
 
 if __name__ == "__main__":
-    quiz(int(input("""
-    Location: """)))
+    print(quiz(int(input()), [None, None, None]))
