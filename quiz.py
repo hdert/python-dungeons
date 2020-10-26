b41c8840fec37a23780353b661665cdc50571e72
hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2020-10-27 12:49:19 +1300

    comment code

--- quiz.py
+++ quiz.py
@@ -5,6 +5,7 @@ from random import randint as rand
 
 def quiz(location, score):
     """D."""
+    # Initialize lists of answers and questions
     quiz_questions = [[
         """Write one of:
     Example
@@ -13,16 +14,22 @@ def quiz(location, score):
     Boom clap I'm in me mom's *"""
     ], [], []]
     quiz_answers = [[["Example", "None", "wasd"], ["car"]], [], []]
+    # get the question answer, and score values to use based on the users
+    # location
     current_questions = quiz_questions[location - 1]
     current_answers = quiz_answers[location - 1]
     score[location - 1] = 0
     while len(current_questions) > 0:
+        # Run while there are still questions left
         rand_choice = rand(0, len(current_questions) - 1)
+        # pick a random question and answer
         user_input = input(f"""
     {current_questions.pop(rand_choice)}
-    : """)
+    : """)  # give the user the randomly selected question,
+        # delete the question from the master list, and take user input
         answers = current_answers.pop(rand_choice)
-        if user_input in answers:
+        # get the answers to the randomly selected question
+        if user_input in answers:  # check if the users input is
             input("""
     You got it right""")
             score[location - 1] += 1
