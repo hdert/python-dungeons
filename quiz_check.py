fbd2952bcb5ac0a348e5f2cd0db6418055e2813a
hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2020-10-27 12:23:52 +1300

    add comments

--- quiz_check.py
+++ quiz_check.py
@@ -3,16 +3,19 @@
 
 def quiz_check(location, score_one, score_two, score_three):
     """Check if there is an unplayed quiz in the users current room."""
-    if ((location == 1 and score_one is not None)
+    if (
+        (location == 1 and score_one is not None)
             or (location == 2 and score_two is not None)
-            or (location == 3 and score_three is not None)):
-        return True
-    return False
+            or (location == 3 and score_three is not None)
+    ):  # checks if the room has a score ∴ checking if the room has been played
+        return False
+    return True
 
 
 if __name__ == "__main__":
-    for x in range(4):
-        for y in range(2):
+    for x in range(4):  # The player location to iterate through
+        for y in range(
+                2):  # Toggles between having a score and not having a score
             print(
-                quiz_check(x, None if y == 1 else 1, None if y == 1 else 1,
-                           None if y == 1 else 1))
+                quiz_check(x, None if y == 0 else 1, None if y == 0 else 1,
+                           None if y == 0 else 1))
