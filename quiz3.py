27e3f67bd5d8fabd9d45fb223ceb3d57b27ac579
hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2020-10-29 10:56:24 +1300

    update docs, add questions and add comments

--- quiz3.py
+++ quiz3.py
@@ -19,15 +19,28 @@ def quiz(location, score):
         3.1 If the answer was right, iterate the user score and display
         'You got it right'.
         3.2 If not display the correct answer using answer_text.
-    If there are 0 questions return the user scores.
+    If there are 0 questions left in the current location, return the user
+    scores.
     """
     # Initialize lists of answers and questions
     quiz_questions = [[
         """The answer is Example:""", """Finish the lyrics:
     Boom clap I'm in me mom's *"""
-    ], [], []]
+    ],
+                      [
+                          "What part of speech is the word jump?",
+                          """What language feature is this:
+    Go clean your room right now this instance you naughty little
+    devil child!""", """What type of poem is this:
+    Go clean your room right
+    now this instance you naughty
+    little devil child!"""
+                      ], []]
     quiz_answers = [[["Example", "None", "wasd", "Not Example", 0],
-                     ["car", "room", "house", "town", 0]], [], []]
+                     ["car", "room", "house", "town", 0]],
+                    [["Noun", "Verb", "Adjective", "Adverb", 1],
+                     ["Hyperbole", "Rhetoric", "Imperative", "Sonnet", 2],
+                     ["Sonnet", "Haiku", "Limerick", "Free verse", 1]], []]
     # get the question answer, and score values to use based on the users
     # location
     current_questions = quiz_questions[location - 1]
@@ -37,9 +50,10 @@ def quiz(location, score):
         # Run while there are still questions left
         rand_choice = rand(0, len(current_questions) - 1)
         # pick a random question and answer
-        answer = current_answers[rand_choice][4]
-        answer_text = current_answers[rand_choice][current_answers[rand_choice]
-                                                   [4]]
+        answer = current_answers[rand_choice][
+            4]  # get the integer that 'points' to the correct answer
+        answer_text = current_answers[rand_choice][
+            answer]  # feed this integer back in to get the text of the answer
         try:
             user_input = int(
                 input(f"""
@@ -48,12 +62,14 @@ def quiz(location, score):
     2) {current_answers[rand_choice].pop(0)}
     3) {current_answers[rand_choice].pop(0)}
     4) {current_answers[rand_choice].pop(0)}
-    [1-4]: """))  # give the user the randomly selected question,
-        except ValueError:
+    [1-4]: """))  # give the user the randomly selected question and possible
+            # answers
+        except ValueError:  # if the user doesn't put in an interger, skip the
+            # question and give them the error message
             input("""
     When prompted, enter one of the numbers 1, 2, 3, 4.
     Each number corresponds to an action printed on screen""")
-            user_input = None
+            user_input = None  # set user_input so the program doesn't break
         # delete the question from the master list, and take user input
         current_answers.pop(rand_choice)
         # get the answers to the randomly selected question
