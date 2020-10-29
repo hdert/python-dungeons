4f6cdfbaa03f3c62d28196669660ff38584cd5c1
hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2020-10-29 15:10:05 +1300

    write main logic, update docstring

--- want_leaderboard_entry.py
+++ want_leaderboard_entry.py
@@ -1,9 +1,32 @@
 """See want_leaderboard_entry.__doc__."""
 
 
-def want_leaderboard_entry():
-    pass
+def want_leaderboard_entry():  # noqa: D400, D205
+    """Deduce whether the user wants their score
+    inserted into the leaderboard.
+
+    Return:
+        A boolean value, True if the user wants their score insert in the
+        leaderboard, False if they do not.
+    """
+    while True:
+        try:
+            user_input = int(
+                input("""
+    Do you want your score saved in the leaderboard:
+    1) Yes
+    2) No
+    [1-2]: """))
+        except ValueError:
+            input("""
+    When prompted, enter one of the numbers 1, 2, 3, 4.
+    Each number corresponds to an action printed on screen""")
+            user_input = None
+        if user_input in (1, 2):
+            if user_input == 1:
+                return True
+            return False
 
 
 if __name__ == "__main__":
-    want_leaderboard_entry()
+    print(want_leaderboard_entry())
