ad26d96a6adef867751e761ca315e17701618d92
hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2020-10-31 09:01:49 +1300

    create customizable error message function

--- out_of_range_error.py
+++ out_of_range_error.py
@@ -1,15 +1,22 @@
 """See out_of_range_error.__doc__."""
 
 
-def out_range_error(length):
+def out_of_range_error(length):  # noqa: D205, D400
+    """Print and out of range error message with the list of numbers based on
+    the variable length.
+
+    Args:
+        length:
+            The length of the listing of valid integer inputs.
+    """
     numbers = []
     for i in range(length):
-        numbers.append(i + 1)
+        numbers.append(str(i + 1))
     input("""
     When prompted, enter one of the numbers {}.
-    Each number corresponds to an action printed on screen""".format(
+    Each number corresponds to an action printed on screen.""".format(
         ", ".join(numbers)))
 
 
 if __name__ == "__main__":
-    print(out_range_error(input()))
+    print(out_of_range_error(int(input())))
