d2d190967dd9d7544b69de23f8e39dfd3e3911ab
hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2020-11-01 18:12:27 +1300

    extract logic to other functions, finish fetch

--- show_leaderboard.py
+++ show_leaderboard.py
@@ -1,5 +1,6 @@
 """See show_leaderboard.__doc__."""
 from out_of_range_error import out_of_range_error
+from user_binary_choice import user_binary_choice
 
 
 def show_leaderboard(c):
@@ -9,41 +10,31 @@ def show_leaderboard(c):
         c:
             The cursor object.
     """
-    while True:
-        try:
-            user_input = int(
-                input("""
-    Do you want to search by username:
-    1) Yes
-    2) No
-    [1-2]: """))
-        except ValueError:
-            out_of_range_error(2)
-            user_input = None
-        if user_input in (1, 2):
-            break
-        out_of_range_error(2)
-    while True:
-        user_input = input("""
+    if user_binary_choice("Do you want to search by username"):
+        while True:
+            user_input = input("""
     Please enter the username: """)
-        if len(user_input) > 15:
-            input("""
+            if len(user_input) > 15:
+                input("""
     The username is too long. The username needs to be under 15 characters""")
-        elif len(user_input) < 1:
-            input("""
+            elif len(user_input) < 1:
+                input("""
     The username is too short. The username needs to be more than 0
     characters""")
-        elif user_input.isalpha():
-            break
-        else:
-            input("""
+            elif user_input.isalpha():
+                break
+            else:
+                input("""
     The username doesn't contain any characters. Please use characters""")
-    if user_input == 1:
-        query = "SELECT * FROM `leaderboard` WHERE `name` LIKE ?"
         user_input = f"%{user_input}%"
+        c.execute(
+            """SELECT * FROM `leaderboard`
+            WHERE `username` LIKE ?
+            ORDER BY `scoreavg`, `username` ASC""", [user_input])
     else:
-        query = "SELECT * FROM `leaderboard`"
-    c.execute(query, )
+        c.execute("""SELECT * FROM `leaderboard`
+            ORDER BY `scoreavg`, `username` ASC""")
+    print(c.fetchall())
 
 
 if __name__ == "__main__":
