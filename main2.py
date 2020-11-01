3fd62a2daa6e00cb8c3279f0fdadbb84df3c7cfd
hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2020-11-01 22:02:22 +1300

    update logic and loops

--- main2.py
+++ main2.py
@@ -11,28 +11,32 @@ from user_binary_choice import user_binary_choice
 
 def main():
     """Run through the entire game."""
-    score = [None, None, None]
-    introduction()
-    location = navigate()
-    while True:
-        if quiz_check(location, score):
-            score = quiz(location, score)
-            if (score[0] is not None and score[1] is not None
-                    and score[2] is not None):
-                if user_binary_choice(
-                        "Do you want your score saved in the leaderboard"):
-                    c = db_create()
-                    leaderboard_entry(c, score)
-                if user_binary_choice("Do you want to see the leaderboard"):
-                    show_leaderboard(c)
-                if user_binary_choice("Do you want to play again"):
-                    return main()
+    user_wants_to_play = True
+    while user_wants_to_play:
+        score = [None, None, None]
+        introduction()
+        location = navigate()
+        while True:
+            if quiz_check(location, score):
+                score = quiz(location, score)
+                if (score[0] is not None and score[1] is not None
+                        and score[2] is not None):
+                    if user_binary_choice(
+                            "Do you want your score saved in the leaderboard"):
+                        c = db_create()
+                        leaderboard_entry(c, score)
+                    if user_binary_choice(
+                            "Do you want to see the leaderboard"):
+                        show_leaderboard(c)
+                    if not user_binary_choice("Do you want to play again"):
+                        user_wants_to_play = False
+                        break
 
-        else:
-            if location != 0:
-                input("""
+            else:
+                if location != 0:
+                    input("""
     You've done this room.""")
-        location = navigate(location)
+            location = navigate(location)
 
 
 if __name__ == "__main__":
