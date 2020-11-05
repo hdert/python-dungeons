fc835fbfcef11ad72d5d5d60c5d6e4ddefba6c0e
hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2020-11-06 07:52:51 +1300

    add comments

--- show_leaderboard2.py
+++ show_leaderboard2.py
@@ -20,36 +20,44 @@ def show_leaderboard(c):
     else:
         c.execute("""SELECT * FROM `leaderboard`
         ORDER BY `scoretotal` DESC, `username` ASC""")
-    stdscr = curses.initscr()
-    curses.noecho()
-    curses.cbreak()
+    stdscr = curses.initscr()  # initialize the curses window
+    curses.noecho()  # make user entered characters not appear
+    curses.cbreak()  # make user input instant
     results = c.fetchall()
     lines = 0
     while lines < len(results):
-        stdscr.addstr('/' + "¯" * 89 + '\\\n')
         stdscr.addstr(
             "| {:15} | {:10} | {:13} | {:11} | {:13} | {:10} |\n".format(
                 'Username', 'Date', 'Overall Score', 'Maths Score',
-                'English Score', 'NCEA Score'))
-        for x in range(curses.LINES - 3):
+                'English Score', 'NCEA Score'))  # print the centered header
+        for x in range(curses.LINES - 2):
+            # do this as many times as the terminal's height - 1
             stdscr.addstr(
                 "| {:15} | {:10} | {:13} | {:11} | {:13} | {:10} |\n".format(
                     results[lines][0], results[lines][1], results[lines][5],
                     results[lines][2], results[lines][3], results[lines][4]))
-            lines += 1
+            # print the results spaced out, line by line
+            lines += 1  # iterate the result line to print out
             if lines >= len(results):
+                # if you reach the end of the results, pause, and end the
+                # program
                 stdscr.addstr("(enter to continue)")
-                stdscr.getkey()
-                curses.endwin()
+                stdscr.getkey()  # equivalent of input() but for one char
+                curses.endwin()  # close the curses window
                 return
         stdscr.addstr("[q]uit, [n]ew page: ")
+        # once the results span the terminal's height - 1 print this prompt
+        stdscr.refresh()
+        # get curses to display the buffered output
         while True:
+            # get the user's input forever until they hit a valid key
             user_input = stdscr.getkey().lower()
-            stdscr.refresh()
             if user_input == 'q':
+                # close the program
                 curses.endwin()
                 return
             if user_input == 'n':
+                # clear the screen and then print a new screen of results
                 stdscr.clear()
                 break
 
