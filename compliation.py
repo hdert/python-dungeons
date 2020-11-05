193c2c8071ae1cc2fa7fe6eb9960d6f6efb24d2a
hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2020-11-05 21:20:29 +1300

    swap in show_leaderboard2

--- compliation.py
+++ compliation.py
@@ -4,7 +4,7 @@ from os.path import isfile
 import sqlite3
 from datetime import date
 from random import randint as rand
-from shutil import get_terminal_size
+import curses
 
 
 def clear_screen():
@@ -186,12 +186,6 @@ def navigate(location=0):  # noqa: D205
 
     relations = [[2, 4, 4, 4], [4, 4, 0, 4], [3, 0, 4, 4], [1, 4, 4, 0]]
 
-    # desc = [
-    #     "A dull enclosed space with three doors",
-    #     "A dull enclosed space with one door",
-    #     "A dull enclosed space with one door",
-    #     "A dull enclosed space with one door"
-    # ]
     try:
         user_input = int(
             input(f"""
@@ -362,35 +356,39 @@ def show_leaderboard(c):
             WHERE `username` LIKE ?
             ORDER BY `scoretotal` DESC, `username` ASC""", [username])
     else:
-        c.execute("""SELECT * FROM `leaderboard`
-            ORDER BY `scoretotal`, `username` ASC""")
-    clear_screen()
+        c.execute("""SELECT * FROM `leaderboard`""")
+    stdscr = curses.initscr()
+    curses.noecho()
+    curses.cbreak()
     results = c.fetchall()
     lines = 0
     while lines < len(results):
-        print('/' + "¯" * 89 + '\\')
-        print(
-            f"| {'Username':15} | {'Date':10} | {'Overall Score':13} | {'Maths Score':11} | {'English Score':13} | {'NCEA Score':10} |"
-        )
-        for x in range(get_terminal_size().lines - 3):
-            line = x + lines
-            print(
-                f"| {results[line][0]:15} | {results[line][1]:10} | {results[line][5]:13} | {results[line][2]:11} | {results[line][3]:13} | {results[line][4]:10} |"
-            )
+        stdscr.addstr('/' + "¯" * 89 + '\\\n')
+        stdscr.addstr(
+            "| {:15} | {:10} | {:13} | {:11} | {:13} | {:10} |\n".format(
+                'Username', 'Date', 'Overall Score', 'Maths Score',
+                'English Score', 'NCEA Score'))
+        for x in range(curses.LINES - 3):
+            stdscr.addstr(
+                "| {:15} | {:10} | {:13} | {:11} | {:13} | {:10} |\n".format(
+                    results[lines][0], results[lines][1], results[lines][5],
+                    results[lines][2], results[lines][3], results[lines][4]))
             lines += 1
-            if lines + x >= len(results):
-                input("(enter to continue)")
-                clear_screen()
+            if lines >= len(results):
+                stdscr.addstr("(enter to continue)")
+                stdscr.getkey()
+                curses.endwin()
                 return
+        stdscr.addstr("[q]uit, [n]ew page: ")
         while True:
-            user_input = input("[q]uit, [n]ew page: ").lower()
+            user_input = stdscr.getkey().lower()
+            stdscr.refresh()
             if user_input == 'q':
-                clear_screen()
+                curses.endwin()
                 return
             if user_input == 'n':
-                clear_screen()
+                stdscr.clear()
                 break
-            print('\033[A', end='')
 
 
 def user_binary_choice(x):  # noqa: D400, D205
